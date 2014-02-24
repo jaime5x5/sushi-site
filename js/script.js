432ab5f1d0cb10edc79029d596b30473b30e7466
Jaime Williams <jaime.williams@eagles.ewu.edu>
2014-02-24 11:57:01 -0800

    fixed js errors for map.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,14 @@
-$(document).foundation();
-$(document).ready(function() {  
-  new Maplace({
-    map_div: '#gmap-styled',
-    locations: [{
-      lat: 47.659381,
-      lon: -117.4236639,
-      zoom: 18
 
-    }],
-    start: 3,
-    styles: {
-      'Other style': [{
-        stylers: [
-            { hue: "#00ffe6" },
-            { saturation: -20 }
-        ]
-      }, {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [
-            { lightness: 100 },
-            { visibility: "simplified" }
-        ]
-      }, {
-        featureType: "road",
-        elementType: "labels",
-        stylers: [
-            { visibility: "off" }
-        ]
-      }],
-      'Night': [{
-        featureType: 'all',
-        stylers: [
-          { invert_lightness: 'true' }
-        ]
-      }],
-      'Greyscale': [{              
-        featureType: 'all',
-        stylers: [
-          { saturation: -100 },
-          { gamma: 0.50 }
-        ]
-      }]
-    }
-  }).Load();
+
+$(document).ready(function() {
+	new Maplace({
+		locations: [{
+			lat: 47.659381,
+			lon: -117.4236639,
+			zoom: 15
+
+		}]
+	}).Load();
+});
+
+$(document).foundation();
